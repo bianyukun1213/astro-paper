522d0b3012758f75649fb9f625ae86d3a6a31fe0
Hollis <bianyukun1213@outlook.com>
2026-04-06 18:41:37 +0800

    update dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -62,6 +62,7 @@ export default defineConfig({
     defaultLocale: "en-US",
     routing: {
       prefixDefaultLocale: true,
+      redirectToDefaultLocale: true,
     },
   },
   env: {
@@ -73,17 +74,14 @@ export default defineConfig({
       }),
     },
   },
-  experimental: {
-    preserveScriptOrder: true,
-    fonts: [
-      {
-        name: "Google Sans Code",
-        cssVariable: "--font-google-sans-code",
-        provider: fontProviders.google(),
-        fallbacks: ["monospace"],
-        weights: [300, 400, 500, 600, 700],
-        styles: ["normal", "italic"],
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Google Sans Code",
+      cssVariable: "--font-google-sans-code",
+      fallbacks: ["monospace"],
+      weights: [300, 400, 500, 600, 700],
+      styles: ["normal", "italic"],
+    },
+  ],
 });
